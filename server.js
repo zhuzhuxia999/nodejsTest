2a37eb0515f7648df477eb826b57e4f142a4507b
wnagyijie <yijie.wang@apicloud.com>
2020-01-08 17:34:15 +0800

    nodeAPIExample

--- server.js
+++ server.js
@@ -7,4 +7,25 @@ http.createServer(function (request,response) {
     response.end('Hello world');
 }).listen(8889);
 
-console.log("欢迎来到 8889 端口")
+console.log("欢迎来到 8889 端口");
+
+//读取文件
+/**
+ * 阻塞 代码示例
+ * @type {module:fs}
+ */
+var fs = require('fs');
+var fileData = fs.readFileSync('fileTest.txt');
+
+console.log(fileData.toString());
+console.log('程序执行结束');
+
+
+/***
+ * 非阻塞代码
+ */
+fs.readFile('fileTest.txt',function (err,data) {
+    if (err) return console.error(err);
+    console.log(data.toString());
+})
+console.log("非阻塞代码执行结束");
